816fb2c00eaf0a6bbbcccf3aac792254a1ef7283
KoalaBear <nswarup14@gmail.com>
2018-02-07 14:57:08 +0530

    Data is now loaded and stored in a variable called dataset

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from matplotlib.pyplot import plot
 from sklearn import datasets
-from csv import *
-
+import csv
+import numpy as np
 
 if __name__=='__main__':
+    
+    dataset=[]            
+    with open('dataA2.csv','r+') as inf:
+        reader= csv.reader(inf, delimiter=',')
+        for row in reader:
+            row_wise=[]
+            for ele in row:
+                row_wise.append(int(ele))
+        
+            dataset.append(row_wise)
 
+    dataset=np.array(dataset)
+    print(dataset)
 
