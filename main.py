dd5bd341c9b77fc7c10ce46956ccdb8b63a8016e
KoalaBear <nswarup14@gmail.com>
2018-02-07 15:19:54 +0530

    PCA of the data is completed. The number of components is 34 for a 0.99 explained_variance_ratio

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from matplotlib.pyplot import plot
+import matplotlib.pyplot as plt
 from sklearn import datasets
 import csv
 import numpy as np
+from sklearn.decomposition import PCA
+
+def doPCA(dataset):
+
+    pca=PCA(0.99)
+    #components is going to store the number of components in the reduced dimensionatlity data
+    dataset=pca.fit_transform(dataset)
+
+    components= dataset[0].size
+    print(components)
+
+    #altered database
+    return dataset, components
 
 if __name__=='__main__':
     
@@ -16,8 +29,10 @@ if __name__=='__main__':
             dataset.append(row_wise)
 
     dataset=np.array(dataset)
-    print(dataset)
-    dataset=dataset[:,:-1]
     target=dataset[:,-1]
-    
+    dataset=dataset[:,:-1]
+
+    #plt.plot(dataset,target)
+    dataset, components =doPCA(dataset)
+
 
