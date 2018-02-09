f1b91790e577808a1442adb12a76605bd00f2c07
Heroin14 <nswarup14@gmail.com>
2018-02-10 03:08:32 +0530

    Gradient boosting algorithm is added to the list

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sklearn.decomposition import PCA
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import pickle
+from sklearn.ensemble import GradientBoostingClassifier
+
 
 def Plotting(dataset, target):
 
@@ -34,6 +36,23 @@ def openFile():
     return f
 def closeFile(f):
     f.close()
+
+
+def GradientBoosting(dataset, target):
+    clf=GradientBoostingClassifier()
+
+    sum=0
+    for i in range(5):
+        X_train,X_test,y_train,y_test=train_test_split(dataset,target, test_size=0.1)
+        clf.fit(X_train,y_train)
+        sum+=clf.score(X_test,y_test)
+    
+    avg=sum/5
+    f=openFile()
+    f.write("GradientBoostingClassfier ")
+    f.write(str(avg))
+    f.write("\n")
+    closeFile(f)
 
 def DecisionTree(dataset , target):
     
@@ -65,7 +84,7 @@ def doPCA(dataset):
     return dataset, components
 
 if __name__=='__main__':
-    
+    """    
     dataset=[]            
     with open('dataA2.csv','r+') as inf:
         reader= csv.reader(inf, delimiter=',')
@@ -86,8 +105,9 @@ if __name__=='__main__':
     pickle_object=open("DatasetPCA","wb")
     pickle.dump([dataset,target],pickle_object)
     pickle_object.close()
-    #Dataset is pickled and kept in the working directory
-    
-    
+    #Dataset is pickled and kept in the working directory 
+    """
+    dataset,target=pickle.load(open("DatasetPCA","rb"))
+    print("asd")
     #DecisionTree(dataset,target)
-
+    #GradientBoosting(dataset,target)
